d6863a92930fe35a591df3ab12153f5974aee870
Roger <logroger@gmail.com>
2021-05-27 16:57:58 -0300

    add Lombok dependency

--- src/main/java/io/github/rogerlog/springbootjwtproject/SpringBootJwtApplication.java
+++ src/main/java/io/github/rogerlog/springbootjwtproject/SpringBootJwtApplication.java
@@ -1,5 +1,6 @@
 package io.github.rogerlog.springbootjwtproject;
 
+import io.github.rogerlog.springbootjwtproject.data.UserData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootJwtApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootJwtApplication.class, args);
+
 	}
 
 }
